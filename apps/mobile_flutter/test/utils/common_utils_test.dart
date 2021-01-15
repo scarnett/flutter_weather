f847490acd265223c8c9f45c1d01f711a508e82d
Scott <scott.carnett@dvt-inc.com>
2021-01-15 12:45:05 -0600

    quick test

---
+++ apps/mobile_flutter/test/utils/common_utils_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_weather/utils/common_utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Should be an integer', () {
+    expect(isInteger(1), true);
+    expect(isInteger(1.0), true);
+    expect(isInteger(-1), true);
+    expect(isInteger(-1.0), true);
+  });
+
+  test('Should NOT be an integer', () {
+    expect(isInteger(0.5), false);
+    expect(isInteger(1.5), false);
+    expect(isInteger(-0.5), false);
+    expect(isInteger(-1.5), false);
+  });
+}
