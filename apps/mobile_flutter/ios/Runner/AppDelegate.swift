2125cecaa40375fc801b1c294fcdc4519a6177fe
Scott <scott.carnett@dvt-inc.com>
2021-01-18 11:52:02 -0600

    ios workflow

--- apps/mobile_flutter/ios/Runner/AppDelegate.swift
+++ apps/mobile_flutter/ios/Runner/AppDelegate.swift
@@ -1,15 +1,18 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  override init() {
+    super.init()
+    FirebaseApp.configure()
+  }
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    var flutter_native_splash = 1
-    UIApplication.shared.isStatusBarHidden = false
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
