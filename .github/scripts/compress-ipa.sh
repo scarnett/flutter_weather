c46a1f6e612b787819bc708aa8fde47efffed9e7
Scott <scott.carnett@dvt-inc.com>
2021-01-22 17:32:25 -0600

    ios workflow

--- .github/scripts/compress-ipa.sh
+++ .github/scripts/compress-ipa.sh
@@ -9,12 +9,17 @@ cd Payload
 ln -s ../Runner.app
 cd ..
 
+for APP in `ls ./Applications/*.app`
+do
+  echo "$APP"
+done
+
 for FRAMEWORK in `ls ./Payload/Runner.app/Frameworks/*.framework`
 do
   basename "$FRAMEWORK"
   fileName="$(basename -- $FRAMEWORK)"
   echo "$fileName"
-  cp "/Applications/Runner.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/$fileName" "./SwiftSupport/iphoneos/$fileName"
+  cp "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/$fileName" "./SwiftSupport/iphoneos/$fileName"
 done
 
 zip -r "$FLUTTER_WEATHER_IPA_OUTPUT_FILE" Payload SwiftSupport
