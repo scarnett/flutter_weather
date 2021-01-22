368d06f64600f2a718ccf3772fc7ece3280bbb88
Scott <scott.carnett@dvt-inc.com>
2021-01-22 17:08:14 -0600

    ios workflow

--- .github/scripts/compress-ipa.sh
+++ .github/scripts/compress-ipa.sh
@@ -5,15 +5,16 @@ set -euo pipefail
 cd "$FLUTTER_WEATHER_IPA_OUTPUT_FOLDER"
 mkdir Payload
 mkdir -p SwiftSupport/iphoneos
-
 cd Payload
 ln -s ../Runner.app
 cd ..
 
 for FRAMEWORK in `ls ./Payload/Runner.app/Frameworks/*.framework`
 do
-  echo "$FRAMEWORK"
-  cp "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/$FRAMEWORK" "./SwiftSupport/iphoneos/$FRAMEWORK"
+  basename "$FRAMEWORK"
+  fileName="$(basename -- $FRAMEWORK)"
+  echo "$fileName"
+  cp "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/$fileName" "./SwiftSupport/iphoneos/$fileName"
 done
 
 zip -r "$FLUTTER_WEATHER_IPA_OUTPUT_FILE" Payload SwiftSupport
