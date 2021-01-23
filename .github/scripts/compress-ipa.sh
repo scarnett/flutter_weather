56925e38f1c7b612d9023faf9334711049c10d7c
Scott <scott.carnett@dvt-inc.com>
2021-01-23 11:32:37 -0600

    ios workflow

--- .github/scripts/compress-ipa.sh
+++ .github/scripts/compress-ipa.sh
@@ -7,5 +7,24 @@ mkdir Payload
 cd Payload
 ln -s ../Runner.app
 cd ..
-zip -r "$FLUTTER_WEATHER_IPA_OUTPUT_FILE" Payload
+
+APP="Payload/Runner.app"
+
+DEVELOPER_DIR=`xcode-select --print-path`
+if [ ! -d "${DEVELOPER_DIR}" ]; then
+  echo "No developer directory found!"
+  exit 1
+fi
+
+echo "+ Adding SWIFT support (if necessary)"
+if [ -d "${APP}/Frameworks" ];
+then
+    mkdir -p SwiftSupport
+    for SWIFT_LIB in $(ls -1 "${APP}/Frameworks/"); do
+        echo "Copying ${SWIFT_LIB}"
+        cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "./SwiftSupport"
+    done
+fi
+
+zip -r "$FLUTTER_WEATHER_IPA_OUTPUT_FILE" Payload SwiftSupport
 ls -l "$FLUTTER_WEATHER_IPA_OUTPUT_FILE"
