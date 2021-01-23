8078316aa558218c89b0abc18fec581f9c9a87da
Scott <scott.carnett@dvt-inc.com>
2021-01-22 19:56:30 -0600

    ios workflow

--- .github/scripts/compress-ipa.sh
+++ .github/scripts/compress-ipa.sh
@@ -8,7 +8,7 @@ mkdir -p SwiftSupport/iphoneos
 cd Payload
 ln -s ../Runner.app
 cd ..
-cp -vr "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/prebuilt-modules/" "./SwiftSupport/iphoneos/"
+cp -vr "$XCODE_SWIFT_PREBUILT_MODULES" "./SwiftSupport/iphoneos/"
 
 zip -r "$FLUTTER_WEATHER_IPA_OUTPUT_FILE" Payload SwiftSupport
 ls -l "$FLUTTER_WEATHER_IPA_OUTPUT_FILE"
