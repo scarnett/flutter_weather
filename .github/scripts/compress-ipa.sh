72f7d0ec6b892d4d032f35ba8b436f0b82c4ac06
Scott <scott.carnett@dvt-inc.com>
2021-01-22 22:00:28 -0600

    ios workflow

--- .github/scripts/compress-ipa.sh
+++ .github/scripts/compress-ipa.sh
@@ -8,9 +8,5 @@ mkdir -p SwiftSupport/iphoneos
 cd Payload
 ln -s ../Runner.app
 cd ..
-cp -vr "$XCODE_SWIFT_LIBS" "./SwiftSupport/iphoneos/"
-rm "./SwiftSupport/iphoneos/libswiftRemoteMirror.dylib"
-find "./SwiftSupport" -name ".DS_Store" -delete
-
 zip -r "$FLUTTER_WEATHER_IPA_OUTPUT_FILE" Payload SwiftSupport
 ls -l "$FLUTTER_WEATHER_IPA_OUTPUT_FILE"
