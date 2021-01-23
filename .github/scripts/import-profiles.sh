1eefb92d13b42306b6ed00097c45378ba7f6b7b8
Scott <scott.carnett@dvt-inc.com>
2021-01-23 10:39:49 -0600

    ios workflow

--- .github/scripts/import-profiles.sh
+++ .github/scripts/import-profiles.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 
 security import "$FLUTTER_WEATHER_CERTS_FILE_PATH" -k "$FLUTTER_WEATHER_KEYCHAIN" -P "$FLUTTER_WEATHER_CERTS_PASSWORD" -A
 security set-key-partition-list -S apple-tool:,apple: -s -k "" "$FLUTTER_WEATHER_KEYCHAIN"
-tar xzvf "$FLUTTER_WEATHER_PROVISION_FILE_PATH"
+
 mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
-for PROVISION in `ls ./*.mobileprovision`
-do
-  UUID=`/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i ./$PROVISION)`
-  cp "./$PROVISION" "$HOME/Library/MobileDevice/Provisioning Profiles/$UUID.mobileprovision"
-done
+echo "$FLUTTER_WEATHER_PROVISION_PROFILE_DATA" | base64 --decode > "$HOME/Library/MobileDevice/Provisioning Profiles/profile.mobileprovision"
