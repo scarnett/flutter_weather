4131d123b2e6c0c23b533a7a6be8de4fb4ea8711
Scott <798305+scarnett@users.noreply.github.com>
2021-01-29 11:33:30 -0600

    firebase workflow test

---
+++ .github/scripts/update-remote-config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+while getopts ":p:v:" opt; do
+  case $opt in
+    p) platform="$OPTARG"
+    ;;
+    v) version="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+# Download the remote configuration template and dump it to a file
+firebase remoteconfig:get -o ../templates/firebase-remote-config.json
+
+# Update some values in the file
+python update-remote-config.py -p "$platform" -v "$version"
+
+# Deploy the updated remote configuration template
+firebase deploy --only remoteconfig
