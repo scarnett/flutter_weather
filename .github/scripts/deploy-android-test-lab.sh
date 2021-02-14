7545352437a09c79d010af5965aff9e696df829d
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-02-14 09:30:29 -0600

    some cleanup

--- .github/scripts/deploy-android-test-lab.sh
+++ .github/scripts/deploy-android-test-lab.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
-gcloud auth activate-service-account --key-file=/home/scott/dev/apps/flutter_weather/.github/scripts/my-flutter-weather-firebase-adminsdk-et7se-99c04b8f4e.json
-gcloud --quiet config set project my-flutter-weather
+gcloud auth activate-service-account --key-file="$FIREBASE_SERVICE_ACCOUNT_MY_FLUTTER_WEATHER"
+gcloud --quiet config set project "$FIREBASE_PROJECT_ID"
 gcloud firebase test android run \
   --type instrumentation \
-  --app /home/scott/dev/apps/flutter_weather/apps/mobile_flutter/build/app/outputs/flutter-apk/app.apk \
-  --test /home/scott/dev/apps/flutter_weather/apps/mobile_flutter/build/app/outputs/apk/androidTest/prod/debug/app-prod-debug-androidTest.apk \
+  --app ./apps/mobile_flutter/build/app/outputs/flutter-apk/app.apk \
+  --test ./apps/mobile_flutter/build/app/outputs/apk/androidTest/prod/debug/app-prod-debug-androidTest.apk \
   --device model=Pixel2,version=29,orientation=portrait \
-  --environment-variables package=io.flutter_weather.prod,debug=false \
+  --environment-variables package="$FIREBASE_ANDROID_PACKAGE_NAME",debug=false \
   --timeout 2m \
   --results-bucket=flutter-weather
 # --results-dir=<RESULTS_DIRECTORY>
