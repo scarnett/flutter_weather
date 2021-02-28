a8eeb08e8d926e599ca7f449646d6af5f0b7af7b
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-02-28 15:44:30 -0600

    download provisioning profile test

--- .github/scripts/import-profiles.sh
+++ .github/scripts/import-profiles.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 security import "$FLUTTER_WEATHER_CERTS_FILE_PATH" -k "$FLUTTER_WEATHER_KEYCHAIN" -P "$FLUTTER_WEATHER_CERTS_PASSWORD" -A
 security set-key-partition-list -S apple-tool:,apple: -s -k "" "$FLUTTER_WEATHER_KEYCHAIN"
-mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
-echo "$1" > "$HOME/Library/MobileDevice/Provisioning Profiles/profile.mobileprovision"
+# mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
+# echo "$1" > "$HOME/Library/MobileDevice/Provisioning Profiles/profile.mobileprovision"
