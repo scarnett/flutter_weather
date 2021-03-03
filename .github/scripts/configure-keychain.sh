38472d726e3eb398c9ef3f6d1b0c186757f2e5f6
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-03-03 17:05:17 -0600

    script test

--- .github/scripts/configure-keychain.sh
+++ .github/scripts/configure-keychain.sh
@@ -10,4 +10,4 @@ security set-keychain-settings
 security list-keychains
 
 sudo security add-trusted-cert -d -r trustRoot -k "$FLUTTER_WEATHER_KEYCHAIN" "./flutterWeather.cer"
-security export -k "$FLUTTER_WEATHER_KEYCHAIN" -t all -f pkcs12 -P "$FLUTTER_WEATHER_CERTS_PASSWORD" -o "$FLUTTER_WEATHER_CERTS_FILE_PATH"
+# security export -k "$FLUTTER_WEATHER_KEYCHAIN" -t all -f pkcs12 -P "$FLUTTER_WEATHER_CERTS_PASSWORD" -o "$FLUTTER_WEATHER_CERTS_FILE_PATH"
