bd0aac45b7380a5e7391ba42860f0578f598d87b
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-03-02 22:46:50 -0600

    arg fixes

--- .github/scripts/create-provisioning-profile.sh
+++ .github/scripts/create-provisioning-profile.sh
@@ -12,10 +12,10 @@ pip install requests Authlib
 
 # Create the provisioning profile
 python .github/scripts/create-provisioning-profile.py \
-  -keyId "$FLUTTER_WEATHER_APPSTORE_KEY_ID" \
-  -issuerId "$FLUTTER_WEATHER_APPSTORE_ISSUER_ID" \
-  -privateKey "$FLUTTER_WEATHER_APPSTORE_PRIVATE_KEY" \
-  -identifier "$FLUTTER_WEATHER_APPSTORE_IDENTIFIER" \
-  -certificateId "$FLUTTER_WEATHER_APPSTORE_CERTIFICATE_ID" \
-  -profileName "$FLUTTER_WEATHER_APPSTORE_PROFILE_NAME" \
-  -profileType "$FLUTTER_WEATHER_APPSTORE_PROFILE_TYPE"
+  --keyId "$FLUTTER_WEATHER_APPSTORE_KEY_ID" \
+  --issuerId "$FLUTTER_WEATHER_APPSTORE_ISSUER_ID" \
+  --privateKey "$FLUTTER_WEATHER_APPSTORE_PRIVATE_KEY" \
+  --identifier "$FLUTTER_WEATHER_APPSTORE_IDENTIFIER" \
+  --certificateId "$FLUTTER_WEATHER_APPSTORE_CERTIFICATE_ID" \
+  --profileName "$FLUTTER_WEATHER_APPSTORE_PROFILE_NAME" \
+  --profileType "$FLUTTER_WEATHER_APPSTORE_PROFILE_TYPE"
