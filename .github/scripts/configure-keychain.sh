fe09bb8b67b8f4032a73c22c9f2d8c5ee0990908
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-03-03 23:09:37 -0600

    script test

--- .github/scripts/configure-keychain.sh
+++ .github/scripts/configure-keychain.sh
@@ -9,7 +9,7 @@ security unlock-keychain -p "" "$FLUTTER_WEATHER_KEYCHAIN"
 security set-keychain-settings
 security list-keychains
 
-security add-trusted-cert -d -r trustRoot -k "$FLUTTER_WEATHER_KEYCHAIN" "./apps/mobile_flutter/ios/flutterWeather.cer" #TODO! cer path
+sudo security add-trusted-cert -d -r trustRoot -k "$FLUTTER_WEATHER_KEYCHAIN" "./apps/mobile_flutter/ios/flutterWeather.cer" #TODO! cer path
 security export -k "$FLUTTER_WEATHER_KEYCHAIN" -t all -f pkcs12 -P "$FLUTTER_WEATHER_CERTS_PASSWORD" -o "$FLUTTER_WEATHER_CERTS_FILE_PATH"
 
 security import "$FLUTTER_WEATHER_CERTS_FILE_PATH" -k "$FLUTTER_WEATHER_KEYCHAIN" -P "$FLUTTER_WEATHER_CERTS_PASSWORD" -T /usr/bin/codesign
