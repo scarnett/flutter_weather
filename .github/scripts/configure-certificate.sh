beec51578d16b3f17351a26392685bb60f0f302b
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-03-04 23:12:36 -0600

    script test

--- .github/scripts/configure-certificate.sh
+++ .github/scripts/configure-certificate.sh
@@ -5,9 +5,6 @@ set -euo pipefail
 # sudo security add-trusted-cert -d -r trustRoot -k "$FLUTTER_WEATHER_KEYCHAIN" "./apps/mobile_flutter/ios/flutterWeather.cer" #TODO! cer path
 # security export -k "$FLUTTER_WEATHER_KEYCHAIN" -t all -f pkcs12 -P "$FLUTTER_WEATHER_CERTS_PASSWORD" -o "$FLUTTER_WEATHER_CERTS_FILE_PATH"
 
-echo "$FLUTTER_WEATHER_CERTS_FILE_PATH"
-echo "$FLUTTER_WEATHER_KEYCHAIN"
-echo "$FLUTTER_WEATHER_CERTS_PASSWORD"
-security import "$FLUTTER_WEATHER_CERTS_FILE_PATH" -k "$FLUTTER_WEATHER_KEYCHAIN" -P "$FLUTTER_WEATHER_CERTS_PASSWORD" -A
+security import "$FLUTTER_WEATHER_CERTS_FILE_PATH" -k "$FLUTTER_WEATHER_KEYCHAIN" -P "$FLUTTER_WEATHER_CERTS_P12_PASSWORD" -A
 security find-identity
 security set-key-partition-list -S apple-tool:,apple: -s -k "" "$FLUTTER_WEATHER_KEYCHAIN"
