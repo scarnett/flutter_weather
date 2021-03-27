185d9f35ae0449493403eb50793e51eb278d80b5
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-03-27 10:21:02 -0500

    details theme

--- apps/mobile_flutter/lib/views/forecast/forecast_details_view.dart
+++ apps/mobile_flutter/lib/views/forecast/forecast_details_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/bloc/bloc.dart';
+import 'package:flutter_weather/widgets/app_ui_overlay_style.dart';
 
 class ForecastDetailsView extends StatelessWidget {
   static Route route() =>
@@ -26,11 +29,42 @@ class ForecastPageView extends StatefulWidget {
 }
 
 class _ForecastDetailsViewState extends State<ForecastPageView> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(
     BuildContext context,
-  ) {
-    // TODO!
-    return Container();
+  ) =>
+      Scaffold(
+        key: _scaffoldKey,
+        extendBody: true,
+        body: BlocListener<AppBloc, AppState>(
+          listener: _blocListener,
+          child: WillPopScope(
+            onWillPop: () => _willPopCallback(context.read<AppBloc>().state),
+            child: _buildBody(context.watch<AppBloc>().state),
+          ),
+        ),
+      );
+
+  void _blocListener(
+    BuildContext context,
+    AppState state,
+  ) {}
+
+  Future<bool> _willPopCallback(
+    AppState state,
+  ) async {
+    // ...
+    return Future.value(true);
   }
+
+  Widget _buildBody(
+    AppState state,
+  ) =>
+      AppUiOverlayStyle(
+        themeMode: state.themeMode,
+        colorTheme: state.colorTheme,
+        child: SafeArea(child: Container()),
+      );
 }
