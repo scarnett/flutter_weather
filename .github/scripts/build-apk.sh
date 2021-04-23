1dda74e43f23923d46c277a8b38bbc389c6138f3
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-04-22 20:20:24 -0500

    some cleanup

--- .github/scripts/build-apk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cd apps/mobile_flutter
-flutter build apk -t lib/main_prod.dart --flavor prod --release --verbose
--- .github/scripts/build-appbundle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-flutter build appbundle -t lib/main_prod.dart --flavor prod --release --verbose
