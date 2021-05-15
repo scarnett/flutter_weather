784c48f32bfb1ad84f1057287f28a438a0af22d9
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-05-15 11:37:36 -0500

    transparent system nav

--- apps/mobile_flutter/android/app/src/main/kotlin/io/flutter_weather/MainActivity.kt
+++ apps/mobile_flutter/android/app/src/main/kotlin/io/flutter_weather/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // @see https://github.com/goderbauer/scratchpad/commit/fb5c97d3ecadf6c48bff20b5a8d83f8b19226070
     override fun onPostResume() {
       super.onPostResume()
       WindowCompat.setDecorFitsSystemWindows(window, false)
