0b59815db09cf46569aca15267788102f8432cf5
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-06-16 09:02:14 -0500

    ad close button

--- apps/mobile_flutter/lib/app/widgets/app_native_ad.dart
+++ apps/mobile_flutter/lib/app/widgets/app_native_ad.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_weather/app/app_config.dart';
+import 'package:flutter_weather/app/bloc/bloc.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:sentry/sentry.dart';
 
@@ -11,6 +13,7 @@ class AppNativeAd extends StatefulWidget {
     required bool isLoaded,
   })? onAdLoaded;
 
+  final Color? backgroundColor;
   final double height;
   final Alignment alignment;
 
@@ -18,7 +21,8 @@ class AppNativeAd extends StatefulWidget {
     Key? key,
     required this.factoryId,
     this.onAdLoaded,
-    this.height: 72.0,
+    this.backgroundColor,
+    this.height: 64.0,
     this.alignment: Alignment.center,
   }) : super(key: key);
 
@@ -30,6 +34,7 @@ class _AppNativeAdState extends State<AppNativeAd> {
   String? _nativeUnitId;
   NativeAd? _ad;
   bool _isLoaded = false;
+  late AdWidget _adWidget;
 
   @override
   void initState() {
@@ -58,6 +63,7 @@ class _AppNativeAdState extends State<AppNativeAd> {
       );
 
       _ad!.load();
+      _adWidget = AdWidget(ad: _ad!);
     }
 
     super.initState();
@@ -75,9 +81,32 @@ class _AppNativeAdState extends State<AppNativeAd> {
   ) =>
       ((_nativeUnitId != null) && (_ad != null) && _isLoaded)
           ? Container(
-              child: AdWidget(ad: _ad!),
+              color: widget.backgroundColor ?? Colors.black.withOpacity(0.1),
+              margin: const EdgeInsets.only(bottom: 10.0),
+              child: Stack(
+                children: [
+                  _buildAd(),
+                  _buildCloseButton(),
+                ],
+              ),
               height: widget.height,
               alignment: widget.alignment,
             )
           : Container();
+
+  Widget _buildAd() => Padding(
+        padding: const EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0),
+        child: _adWidget,
+      );
+
+  Widget _buildCloseButton() => Padding(
+        padding: const EdgeInsets.only(right: 2.0, top: 2.0),
+        child: GestureDetector(
+          onTap: () => context.read<AppBloc>().add(SetShowPremiumInfo(true)),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: const Icon(Icons.close, size: 16.0),
+          ),
+        ),
+      );
 }
