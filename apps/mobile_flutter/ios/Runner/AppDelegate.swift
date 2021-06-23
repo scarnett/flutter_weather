bb566ac94c0d028fb1f71d28dfde296996513c7c
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-06-22 23:35:32 -0500

    ios updates

---
+++ apps/mobile_flutter/ios/Runner/AppDelegate.swift
@@ -0,0 +1,22 @@
+import UIKit
+import Flutter
+import Firebase
+import google_mobile_ads
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    // GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ kGADSimulatorID as! String ]
+    GADMobileAds.sharedInstance().start(completionHandler: nil)
+    GeneratedPluginRegistrant.register(with: self)
+
+    let appListTileFactory = AppListTileNativeAdFactory()
+    FLTGoogleMobileAdsPlugin.registerNativeAdFactory(self, factoryId: "listTile", nativeAdFactory: appListTileFactory)
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
