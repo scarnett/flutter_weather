599c9cf3e76cc8465b6bf02bfaf8b0fd620ce0a8
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-06-26 12:32:30 -0500

    quick refactor

--- apps/mobile_flutter/ios/Runner/AppNativeAdFactory.swift
+++ apps/mobile_flutter/ios/Runner/AppNativeAdFactory.swift
@@ -38,9 +38,9 @@ class AppListTileNativeAdFactory : FLTNativeAdFactory {
         Scanner(string: colorHex).scanHexInt64(&rgbValue)
 
         return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            red: (CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0),
+            green: (CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0),
+            blue: (CGFloat(rgbValue & 0x0000FF) / 255.0),
             alpha: CGFloat(1.0)
         )
     }
