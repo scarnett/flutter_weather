aa45c6be1d9aa2fa3812fb679ef5ea0807e3eb0a
Scott Carnett <798305+scarnett@users.noreply.github.com>
2021-07-16 11:56:34 -0500

    test tz fix

--- apps/mobile_flutter/test/app/utils/date_utils_test.dart
+++ apps/mobile_flutter/test/app/utils/date_utils_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   test('Should format an epoch date', () {
-    expect(formatEpoch(epoch: 1626386790), '7/15/21 at 05:06 PM');
+    expect(formatEpoch(epoch: 1626386790), '7/15/21 at 10:06 PM');
     expect(formatEpoch(epoch: 1626386790, format: 'M/d/yy'), '7/15/21');
   });
 }
